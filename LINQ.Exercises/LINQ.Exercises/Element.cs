390bdbc3b9ae0cfcc06051441b95d97132f048b7
Harry Nguon <harrynguon@gmail.com>
2022-05-05 21:57:55 +1200

    Element functions completed

--- LINQ.Exercises/LINQ.Exercises/Element.cs
+++ LINQ.Exercises/LINQ.Exercises/Element.cs
@@ -24,7 +24,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void First_n_less_than_0()
 		{
-			var result = TestData.Numbers.First();
+			var result = TestData.Numbers.First(n => n < 0);
 
 			Assert.AreEqual(-3, result);
 		}
@@ -32,7 +32,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Last_n_greater_than_0()
 		{
-			var result = TestData.Numbers.Last();
+			var result = TestData.Numbers.Last(n => n > 0);
 
 			Assert.AreEqual(5, result);
 		}
@@ -40,7 +40,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void First_even_n()
 		{
-			var result = TestData.Numbers.First();
+			var result = TestData.Numbers.First(n => n % 2 == 0);
 
 			Assert.AreEqual(2, result);
 		}
@@ -48,7 +48,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Last_even_n()
 		{
-			var result = TestData.Numbers.Last();
+			var result = TestData.Numbers.Last(n => n % 2 == 0);
 
 			Assert.AreEqual(-4, result);
 		}
@@ -56,7 +56,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void First_n_greater_than_10_if_not_found_return_0()
 		{
-			var result = TestData.Numbers.First();
+			var result = TestData.Numbers.FirstOrDefault(n => n > 10);
 
 			Assert.AreEqual(0, result);
 		}
@@ -64,7 +64,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Last_n_less_than_minus_1234_if_not_found_return_0()
 		{
-			var result = TestData.Numbers.Last();
+			var result = TestData.Numbers.LastOrDefault(n => n < (-1234));
 
 			Assert.AreEqual(0, result);
 		}
@@ -72,7 +72,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Last_elephant()
 		{
-			var result = TestData.Animals.Last();
+			var result = TestData.Animals.Last(a => a == "elephant");
 
 			Assert.AreEqual("elephant", result);
 		}
@@ -80,7 +80,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void First_string_having_4_letters()
 		{
-			var result = TestData.Animals.First();
+			var result = TestData.Animals.First(a => a.Length == 4);
 
 			Assert.AreEqual("lion", result);
 		}
@@ -88,7 +88,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Last_string_containg_g()
 		{
-			var result = TestData.Animals.Last();
+			var result = TestData.Animals.Last(a => a.Contains("g"));
 
 			Assert.AreEqual("penguin", result);
 		}
@@ -96,7 +96,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void First_string_having_s_as_first_letter()
 		{
-			var result = TestData.Animals.First();
+			var result = TestData.Animals.First(a => a[0] == 's');
 
 			Assert.AreEqual("swordfish", result);
 		}
@@ -104,7 +104,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Last_three_letter_long_word_or_null()
 		{
-			var result = TestData.Animals.Last(s => s.Length == 3);
+			var result = TestData.Animals.LastOrDefault(s => s.Length == 3);
 
 			Assert.AreEqual(null, result);
 		}
@@ -112,7 +112,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void First_person_born_after_2000()
 		{
-			var result = TestData.People.First();
+			var result = TestData.People.First(p => p.Born.Year > 2000);
 
 			Assert.AreEqual(TestData.People[2], result);
 		}
@@ -120,7 +120,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Last_person_with_lastname_ending_with_l()
 		{
-			var result = TestData.People.Last();
+			var result = TestData.People.LastOrDefault(p => p.LastName[p.LastName.Length-1] == 'l');
 
 			Assert.AreEqual(TestData.People[2], result);
 		}
@@ -128,7 +128,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void First_person_born_141th_day_of_year()
 		{
-			var result = TestData.People.First();
+			var result = TestData.People.First(p => p.Born.DayOfYear == 141);
 
 			Assert.AreEqual(TestData.People[2], result);
 		}
@@ -136,7 +136,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Last_person_whose_firstname_does_not_start_with_J_or_null()
 		{
-			var result = TestData.People.Last();
+			var result = TestData.People.LastOrDefault(p => !p.FirstName.StartsWith("J"));
 
 			Assert.AreEqual(null, result);
 		}
