eb44cde5ebbc8f81428859cc7d18f73301c87967
Harry Nguon <harrynguon@gmail.com>
2022-05-05 21:49:02 +1200

    Aggregate functions done

--- LINQ.Exercises/LINQ.Exercises/Aggregate.cs
+++ LINQ.Exercises/LINQ.Exercises/Aggregate.cs
@@ -4,13 +4,13 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LINQ.Exercises
 {
-    /// <summary>
-    ///   Your task is to apply LINQ `Count/Sum/Min/Max/Average/Aggregate` methods, so all tests will be passed.
-    ///   Make sure to preview test data located in TestData.cs file.
-    ///   Don't modify assertions or test data, all you need to do is to apply LINQ method so `result` variable will have
-    ///   expected value(s).
-    /// </summary>
-    [TestClass]
+	/// <summary>
+	///   Your task is to apply LINQ `Count/Sum/Min/Max/Average/Aggregate` methods, so all tests will be passed.
+	///   Make sure to preview test data located in TestData.cs file.
+	///   Don't modify assertions or test data, all you need to do is to apply LINQ method so `result` variable will have
+	///   expected value(s).
+	/// </summary>
+	[TestClass]
 	public class Aggregate
 	{
 		[TestMethod]
@@ -25,7 +25,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Count_all_occurences_of_1()
 		{
-			var result = TestData.Numbers.Count();
+			var result = TestData.Numbers.Count(n => n == 1);
 
 			Assert.AreEqual(2, result);
 		}
@@ -34,7 +34,7 @@ namespace LINQ.Exercises
 		public void Count_all_animals_having_character_count_equal_to_5()
 		{
 			// Hint: use nested count
-			var result = TestData.Animals.Count();
+			var result = TestData.Animals.Count(animal => animal.Length == 5);
 
 			Assert.AreEqual(2, result);
 		}
@@ -42,7 +42,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Sum_all_numbers()
 		{
-			var result = TestData.Numbers.Count();
+			var result = TestData.Numbers.Sum();
 
 			Assert.AreEqual(-2, result);
 		}
@@ -50,7 +50,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Sum_all_characters_in_animal_names()
 		{
-			var result = TestData.Animals.Count();
+			var result = TestData.Animals.Sum(animal => animal.Length);
 
 			Assert.AreEqual(38, result);
 		}
@@ -58,7 +58,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Sum_all_birth_years()
 		{
-			var result = TestData.People.Count();
+			var result = TestData.People.Sum(p => p.Born.Year);
 
 			Assert.AreEqual(7915, result);
 		}
@@ -66,7 +66,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Find_minimum_number()
 		{
-			var result = TestData.Numbers.Count();
+			var result = TestData.Numbers.Min();
 
 			Assert.AreEqual(-5, result);
 		}
@@ -74,7 +74,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Find_length_of_shortest_animal_name()
 		{
-			var result = TestData.Animals.Count();
+			var result = TestData.Animals.Min(a => a.Length);
 
 			Assert.AreEqual(4, result);
 		}
@@ -82,7 +82,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Find_earliest_birthday()
 		{
-			var result = TestData.People.First().Born;
+			var result = TestData.People.Min(p => p.Born);
 
 			Assert.AreEqual(new DateTime(1950, 12, 1), result);
 		}
@@ -90,7 +90,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Find_maximum_number()
 		{
-			var result = TestData.Numbers.Count();
+			var result = TestData.Numbers.Max();
 
 			Assert.AreEqual(5, result);
 		}
@@ -98,7 +98,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Find_length_of_longest_animal_name()
 		{
-			var result = TestData.Animals.Count();
+			var result = TestData.Animals.Max(a => a.Length);
 
 			Assert.AreEqual(9, result);
 		}
@@ -106,7 +106,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Find_latest_birthday()
 		{
-			var result = TestData.People.First().Born;
+			var result = TestData.People.Max(p => p.Born);
 
 			Assert.AreEqual(new DateTime(2001, 5, 21), result);
 		}
@@ -114,7 +114,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Find_average_of_numbers()
 		{
-			double result = TestData.Numbers.Count();
+			double result = TestData.Numbers.Average();
 
 			Assert.AreEqual(-0.2, result);
 		}
@@ -122,7 +122,7 @@ namespace LINQ.Exercises
 		[TestMethod]
 		public void Find_average_of_birth_years()
 		{
-			double result = TestData.People.Count();
+			double result = TestData.People.Average(p => p.Born.Year);
 
 			Assert.AreEqual(1978.75, result);
 		}
@@ -141,7 +141,7 @@ namespace LINQ.Exercises
 		public void Aggregate_Product_of_all_numbers()
 		{
 			// Hint: product is a result of multiplication
-			var result = TestData.Numbers.Aggregate((product, nextValue) => 1);
+			var result = TestData.Numbers.Aggregate((product, nextValue) => product * nextValue);
 
 			Assert.AreEqual(-1800, result);
 		}
@@ -155,7 +155,9 @@ namespace LINQ.Exercises
 			// if this day is bigger than 15, then substract 10 from it
 			// else add 5 to it
 			// and add resulting number to your aggregate
-			var result = TestData.People.Aggregate(0, (sum, person) => 1);
+			var result =
+				TestData.People.Aggregate(256,
+					(acc, p) => acc + (p.Born.Day > 15 ? p.Born.Day - 10 : p.Born.Day + 5));
 
 			Assert.AreEqual(296, result);
 		}
